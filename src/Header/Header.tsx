3792618affc4a5f98c32535d94583a4c4d98ea16
gedamswiti <switigedam17@gmail.com>
2023-05-17 13:06:20 +0530

    new repo data added

---
+++ src/Header/Header.tsx
@@ -0,0 +1,27 @@
+// import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+      <div>
+        <h1>
+            Header
+        </h1>
+         <ul>
+        <li>
+            <a href="/">Home</a>    
+        </li>
+        <li>
+            <a href="/main">Main</a>    
+        </li>
+        <li>
+            <a href="/Edit">Edit</a>    
+        </li>
+        <li>
+            <a href="/View">View</a>    
+        </li>
+       </ul> 
+      </div>
+    )
+};
+
+export default Header;
